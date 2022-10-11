2292bcc5de842d67af236e0f43d2d5db19470fa1
MargaritaWang <112512721+Margaritajellyfish@users.noreply.github.com>
2022-10-11 07:29:15 +0000

    7lab8

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ numbers = list(map(int, input().split()))
 while True:
     for i in range(len(numbers)):
         if numbers[i] < numbers[i-1]:
-            numbers[i-1, i] = numbers[i, i-1]
+            (numbers[i-1], numbers[i]) = (numbers[i], numbers[i-1])
     if numbers[0] < numbers[1] < numbers[2] < numbers[3] < numbers[4]:
         break
 a = int(input())
 for i in range(5):
     if a < numbers[i]:
         numbers.insert(i, a)
+        break
 print(numbers)
